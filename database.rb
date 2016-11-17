904c4853940be7510d5bf2bf2eac2fd12448a101
Kenia Valladarez <keniavalladarez@gmail.com>
2016-11-17 13:31:50 -0500

    Cleaned up linter warnings

--- database.rb
+++ database.rb
@@ -3,9 +3,9 @@ class RunDatabase
     @people = []
   end
 
-#Banner styling
+  # Banner styling
   def banner(message)
-    2.times do
+    1.times do
       puts
     end
 
@@ -13,13 +13,13 @@ class RunDatabase
     puts "!😱  #{message} 😱 !"
     puts "*" * (8 + message.length)
 
-    2.times do
+    1.times do
       puts
     end
   end
 
   def banner_two(message)
-    2.times do
+    1.times do
       puts
     end
 
@@ -27,25 +27,25 @@ class RunDatabase
     puts "| #{message} |"
     puts "_" * (4 + message.length)
 
-    2.times do
+    1.times do
       puts
     end
   end
 
-#Initial app question
+  # Initial app question
   def promt_for_answer
     puts "There are #{@people.length} people in my amazing database"
     banner_two "Would you like to add (A), search (S), or delete (D)? "
     gets.chomp.upcase
   end
 
-# Running the app
+  # Running the app
   def run
     loop do
       answer = promt_for_answer
       case answer
 
-#When user wants to Add
+        # When user wants to Add
         when "A"
           person = Person.new
 
@@ -72,52 +72,52 @@ class RunDatabase
 
           @people << person
 
-# Looping to search for the indexes withing the @people array and comparing it with the users input. Printing out object and its methods
+          # Looping to search for the indexes withing the @people array and comparing it with the users input. Printing out object and its methods
         when "S"
-            print "What name do you want to search for? "
-            search_name = gets.chomp
-            index = 0
-            loop do
-              if index >= @people.length
-                banner "I never found #{search_name}"
-                break
+          print "What name do you want to search for? "
+          search_name = gets.chomp
+          index = 0
+          loop do
+            if index >= @people.length
+              banner "I never found #{search_name}"
+              break
               end
-              person = @people[index]
-              if search_name == person.name
-                puts "Got em!"
-                puts person.name
-                puts person.phone_number
-                puts person.address
-                puts person.position
-                puts person.slack_account
-                puts person.github_account
-                break
-              end
-
-              index = index + 1
+            person = @people[index]
+            if search_name == person.name
+              puts "Got em!"
+              puts person.name
+              puts person.phone_number
+              puts person.address
+              puts person.position
+              puts person.slack_account
+              puts person.github_account
+              break
             end
 
-# Looping to search for the indexes withing the @people array and comparing it with the users input. Deleted the object and it's related methods
+            index += 1
+          end
+
+          # Looping to search for the indexes withing the @people array and comparing it with the users input. Deleted the object and it's related methods
         when "D"
-            print "What name do you want to delete? "
-            delete_name = gets.chomp
+          print "What name do you want to delete? "
+          delete_name = gets.chomp
 
-            index = 0
-            loop do
-              if index >= @people.length
-                banner "Never found #{delete_name}"
-                break
-              end
-
-              person = @people[index]
-              if delete_name == person.name
-                @people.delete(person)
-                break
-              end
-
-              index = index + 1
+          index = 0
+          loop do
+            if index >= @people.length
+              banner "Never found #{delete_name}"
+              break
             end
-# When entry is not valid
+
+            person = @people[index]
+            if delete_name == person.name
+              @people.delete(person)
+              break
+            end
+
+            index += 1
+          end
+          # When entry is not valid
         else
           banner "Not a valid entry. Please try again and press Enter Key."
       end
